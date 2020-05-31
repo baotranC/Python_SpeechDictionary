bac18bf065e510a0ed71b539f2e0bdb91f13b599
Bao <bao-tran.cong-huyen-ton-nu.1@ens.etsmtl.ca>
2020-05-31 00:12:34 -0400

    Add dictionary

--- SpeechDictionary.py
+++ SpeechDictionary.py
@@ -4,8 +4,37 @@ import pyttsx3
 
 def convertText2Audio(audioString):
     engine = pyttsx3.init()
+    
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', 150)
+    
     engine.say(audioString)
     engine.runAndWait()
     
-
-convertText2Audio('hi')
+    
+searchWord = input('What word do you want to search?')
+try:
+    count = 1
+    
+    dictionary = PyDictionary(searchWord)
+    meanings = dictionary.getMeanings()
+    #print(meanings)
+    
+    convertText2Audio("The word is "+searchWord)
+    
+    meanings = meanings[searchWord]
+    # loop to say forst definition , second defintion 
+    for wordClass,definition in meanings.items():
+        convertText2Audio("definition "+str(count))
+        
+        convertText2Audio("word class")
+        print(wordClass)
+        convertText2Audio(wordClass)
+        
+        convertText2Audio("definition")
+        print(definition)
+        convertText2Audio(definition)
+        
+        count = count + 1
+except:
+    convertText2Audio("The word "+searchWord+" is not found")
