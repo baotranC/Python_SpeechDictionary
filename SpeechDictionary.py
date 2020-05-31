bc39dc3e96f90b20dcc21cb834f37ef81cd1ca17
Bao <bao-tran.cong-huyen-ton-nu.1@ens.etsmtl.ca>
2020-05-30 22:30:08 -0400

    Merge Text2SpeechBranch

--- SpeechDictionary.py
+++ SpeechDictionary.py
@@ -1,3 +1,11 @@
 
 from PyDictionary import PyDictionary
 import pyttsx3
+
+def convertText2Audio(audioString):
+    engine = pyttsx3.init()
+    engine.say(audioString)
+    engine.runAndWait()
+    
+
+convertText2Audio('hi')
