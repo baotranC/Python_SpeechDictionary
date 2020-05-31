a816f49cf2d4bddefb2730bc984d3aa8e9852186
Bao <bao-tran.cong-huyen-ton-nu.1@ens.etsmtl.ca>
2020-05-31 12:52:08 -0400

    Change structure

--- README.md
+++ README.md
@@ -5,7 +5,4 @@ SpeechDictionary allows you to find definitions for a word.
 The application uses PyDictionary module to find the definition of the word and pyttsx3 library to translate texts into speech
 
 Here are the features left to do:
-- word translation
-- synonymous
-- antonym
 - translate speech into texts
--- SpeechDictionary.py
+++ SpeechDictionary.py
@@ -2,54 +2,44 @@
 from PyDictionary import PyDictionary
 from bs4 import BeautifulSoup
 import pyttsx3
+import requests
+
+activateSpeech = True 
 
 def convertText2Audio(audioString):
-    engine = pyttsx3.init()
-    
-    rate = engine.getProperty('rate')
-    engine.setProperty('rate', 150)
-    
-    engine.say(audioString)
-    engine.runAndWait()
-    
-    
-def findDefintions(word):
-    try:
-        count = 1
-        dictionary = PyDictionary(searchWord)
-        meanings = dictionary.getMeanings()
+    if activateSpeech: 
+        engine = pyttsx3.init()
         
-        print("The word is: "+searchWord)
-        convertText2Audio("The word is "+searchWord)
+        rate = engine.getProperty('rate')
+        engine.setProperty('rate', 150)
         
-        meanings = meanings[searchWord]
-        for wordClass, definitions in meanings.items():
+        engine.say(audioString)
+        engine.runAndWait()
+    
+def findDefintions(dictionary):
+    count = 1
+    meanings = dictionary.getMeanings()
+    meanings = meanings[searchWord]
+    for wordClass, definitions in meanings.items():
             
-            print("\tClass: "+wordClass)
-            convertText2Audio("\tClass: "+wordClass)
+        print('\tClass: '+wordClass)
+        convertText2Audio('\tClass: '+wordClass)
             
-            #print(definitions) # debug
-            for definition in definitions: 
-                print("\t\tDefinition "+str(count) + ": " + definition)
-                convertText2Audio("\t\tDefinition "+str(count) + ": " + definition)
-                count = count + 1
-    except:
-        print("The word "+searchWord+" is not found")
-        convertText2Audio("The word "+searchWord+" is not found")
+        #print(definitions) # debug
+        for definition in definitions: 
+            print('\t\tDefinition '+str(count) + ': ' + definition)
+            convertText2Audio('\t\tDefinition '+str(count) + ': ' + definition)
+            count = count + 1
 
-
+convertText2Audio('What word do you want to search? ') 
 searchWord = input('What word do you want to search? ')
-print('')
-print('1: Find definitions')
-print('2: Find synonyms')
-print('3: Find antonyms')
-print('4: Translate')
-print('')
-option = input('What do you want to do: ')
 
-if option == '1': 
-    findDefintions(searchWord)
-else: 
-    print("Option "+option+" does not exist")
-    convertText2Audio("Option "+option+" does not exist")
+print('The word is: '+searchWord)
+convertText2Audio('The word is '+searchWord)   
 
+try:
+    dictionaryy = PyDictionary(searchWord) 
+    findDefintions(dictionaryy)
+except:
+        print('The word '+searchWord+' is not found')
+        convertText2Audio('The word '+searchWord+' is not found')
