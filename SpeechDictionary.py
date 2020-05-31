7ffb5ea8dc7322b679cd0a1705d81c5c8cec4f89
Bao <bao-tran.cong-huyen-ton-nu.1@ens.etsmtl.ca>
2020-05-31 11:59:58 -0400

    Fix parser warning and structure code

--- SpeechDictionary.py
+++ SpeechDictionary.py
@@ -1,5 +1,6 @@
 
 from PyDictionary import PyDictionary
+from bs4 import BeautifulSoup
 import pyttsx3
 
 def convertText2Audio(audioString):
@@ -12,28 +13,43 @@ def convertText2Audio(audioString):
     engine.runAndWait()
     
     
-searchWord = input('What word do you want to search?')
-try:
-    count = 1
-    
-    dictionary = PyDictionary(searchWord)
-    meanings = dictionary.getMeanings()
-    #print(meanings)
-    
-    convertText2Audio("The word is "+searchWord)
-    
-    meanings = meanings[searchWord]
-    for wordClass, definitions in meanings.items():
-         
-        convertText2Audio("word class")
-        print(wordClass)
-        convertText2Audio(wordClass)
+def findDefintions(word):
+    try:
+        count = 1
+        dictionary = PyDictionary(searchWord)
+        meanings = dictionary.getMeanings()
         
-        print(definitions) # debug
-        for definition in definitions: 
-            print(str(count) + ": " + definition)
-            convertText2Audio("definition "+str(count))
-            convertText2Audio(definition)
-            count = count + 1
-except:
-    convertText2Audio("The word "+searchWord+" is not found")
+        print("The word is: "+searchWord)
+        convertText2Audio("The word is "+searchWord)
+        
+        meanings = meanings[searchWord]
+        for wordClass, definitions in meanings.items():
+            
+            print("\tClass: "+wordClass)
+            convertText2Audio("\tClass: "+wordClass)
+            
+            #print(definitions) # debug
+            for definition in definitions: 
+                print("\t\tDefinition "+str(count) + ": " + definition)
+                convertText2Audio("\t\tDefinition "+str(count) + ": " + definition)
+                count = count + 1
+    except:
+        print("The word "+searchWord+" is not found")
+        convertText2Audio("The word "+searchWord+" is not found")
+
+
+searchWord = input('What word do you want to search? ')
+print('')
+print('1: Find definitions')
+print('2: Find synonyms')
+print('3: Find antonyms')
+print('4: Translate')
+print('')
+option = input('What do you want to do: ')
+
+if option == '1': 
+    findDefintions(searchWord)
+else: 
+    print("Option "+option+" does not exist")
+    convertText2Audio("Option "+option+" does not exist")
+
